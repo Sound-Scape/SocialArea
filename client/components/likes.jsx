22c0632536ac9c908929d49a45e545b537971262
Alan <alanfu@live.com>
2018-09-10 14:14:52 -0700

    Added all components to the main App.jsx

--- client/components/likes.jsx
+++ client/components/likes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Container extends React.Component {
+class Likes extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
@@ -9,9 +9,9 @@ class Container extends React.Component {
   render() {
     return (
       <div>
-        2.
+        5.
         {' '}
-        <b>Container</b>
+        <b>Likes</b>
         {' '}
 Component is operational...
       </div>
@@ -19,4 +19,4 @@ Component is operational...
   }
 }
 
-export default Container;
+export default Likes;
--- client/components/share.jsx
+++ client/components/share.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Container extends React.Component {
+class Share extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
@@ -9,9 +9,9 @@ class Container extends React.Component {
   render() {
     return (
       <div>
-        2.
+        7.
         {' '}
-        <b>Container</b>
+        <b>Share</b>
         {' '}
 Component is operational...
       </div>
@@ -19,4 +19,4 @@ Component is operational...
   }
 }
 
-export default Container;
+export default Share;
