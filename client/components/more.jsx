9153655e94744c821ec1a176149f4512c6bbc217
gmhawk <alanfu@live.com>
2018-09-13 11:16:29 -0700

    Buttons turn orange when clicked

--- client/components/more.jsx
+++ client/components/more.jsx
@@ -3,24 +3,46 @@ import React from 'react';
 class More extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      clicked: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      clicked: !prevState.clicked,
+    }));
   }
 
   render() {
+    /* * * * Conditional rendering * * * */
+    let page;
+    let coloring;
+    const { clicked } = this.state;
+    if (clicked === false) {
+      page = '●●● More';
+      coloring = 'black';
+    }
+    if (clicked === true) {
+      page = '●●● More';
+      coloring = '#f50';
+    }
     /* * * * * * Styling * * * * * */
     const styling = {
       position: 'absolute',
-      width: 70,
+      width: 80,
       height: 25,
       bottom: 30,
       left: 270,
+      color: coloring,
       textAlign: 'center',
       backgroundColor: 'white',
       border: '1px solid lightgrey',
     };
     return (
       <div>
-        <button type="button" style={styling}>More</button>
+        <button type="button" style={styling} onClick={this.handleClick}>{page}</button>
       </div>
     );
   }
--- client/components/reposts.jsx
+++ client/components/reposts.jsx
@@ -3,10 +3,31 @@ import React from 'react';
 class Reposts extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      reposted: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      reposted: !prevState.reposted,
+    }));
   }
 
   render() {
+    /* * * * Conditional rendering * * * */
+    let page;
+    let coloring;
+    const { reposted } = this.state;
+    if (reposted === false) {
+      page = 'Repost';
+      coloring = 'black';
+    }
+    if (reposted === true) {
+      page = 'Reposted';
+      coloring = '#f50';
+    }
     /* * * * * * Styling * * * * * */
     const styling = {
       position: 'absolute',
@@ -14,13 +35,16 @@ class Reposts extends React.Component {
       height: 25,
       bottom: 30,
       left: 90,
+      color: coloring,
       textAlign: 'center',
       backgroundColor: 'white',
       border: '1px solid lightgrey',
     };
     return (
       <div>
-        <button type="button" style={styling}>Repost</button>
+        <button type="button" style={styling} onClick={this.handleClick}>
+          {page}
+        </button>
       </div>
     );
   }
