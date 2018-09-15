8eef73ca7070306098ea956226621be6c662cec7
gmhawk <alanfu@live.com>
2018-09-15 09:55:02 -0700

    Added social media icons

--- client/components/reposts.jsx
+++ client/components/reposts.jsx
@@ -76,12 +76,15 @@ function SocialMedia(props) {
     textAlign: 'center',
     backgroundColor: 'white',
     border: '1px solid lightgrey',
-    borderRadius: '12px',
+    borderRadius: '4px',
     lineHeight: '35px',
   };
   const innerStyling = {
-    margin: 12,
-    textAlign: 'center',
+    marginTop: 8,
+    marginLeft: 15,
+    marginRight: 15,
+    marginBottom: 15,
+    textAlign: 'left',
     lineHeight: '20px',
   };
   const buttonStyling = {
@@ -105,6 +108,48 @@ function SocialMedia(props) {
     left: 40,
     bottom: -5.5,
   };
+  const twitter = {
+    position: 'absolute',
+    height: 31,
+    width: 32,
+    left: 25,
+    bottom: 39,
+  };
+  const facebook = {
+    position: 'absolute',
+    height: 31,
+    width: 32,
+    left: 64,
+    bottom: 39,
+  };
+  const tumblr = {
+    position: 'absolute',
+    height: 31,
+    width: 32,
+    left: 102.5,
+    bottom: 39,
+  };
+  const google = {
+    position: 'absolute',
+    height: 31,
+    width: 32,
+    left: 141,
+    bottom: 39,
+  };
+  const pinterest = {
+    position: 'absolute',
+    height: 31,
+    width: 32,
+    left: 180,
+    bottom: 39,
+  };
+  const telegram = {
+    position: 'absolute',
+    height: 31,
+    width: 32,
+    left: 219,
+    bottom: 39,
+  };
   return (
     <div style={styling}>
       <div style={innerStyling}>
@@ -112,7 +157,12 @@ function SocialMedia(props) {
         Would you like to share it with other friends as well?
       </div>
       <div id="social-icons">
-        [Social-Icons]
+        <input type="image" src="https://seeklogo.com/images/T/twitter-icon-square-logo-108D17D373-seeklogo.com.png" alt="twitter_share_icon" style={twitter} />
+        <input type="image" src="http://files.softicons.com/download/social-media-icons/simple-icons-by-dan-leech/png/512x512/facebook.png" alt="facebook_share_icon" style={facebook} />
+        <input type="image" src="https://vectors.pro/wp-content/uploads/2018/04/tumblr-icon-logo-vector-square.png" alt="tumblr_share_icon" style={tumblr} />
+        <input type="image" src="https://pourron.com/wp-content/uploads/2015/09/nouveau-logo-google-plus-carre-petit.png" alt="google_share_icon" style={google} />
+        <input type="image" src="https://uploads-ssl.webflow.com/5af3422365afd71a6f4ab395/5b1983503b6fc4cbfa5287c1_pinterest-p-500.png" alt="pinterest_share_icon" style={pinterest} />
+        <input type="image" src="https://cdn1.iconfinder.com/data/icons/social-black-buttons/512/telegram-512.png" alt="email_share_icon" style={telegram} />
       </div>
       <input type="checkbox" onClick={props.handleAsk} style={checkboxStyling} />
       <p style={checkboxWordStyle}>Don&apos;t ask me again</p>
